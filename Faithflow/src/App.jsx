faff1cfe368bb470ff95e63474d3f4920947cff8
Felipe Castro <77942887+Felpo123@users.noreply.github.com>
2023-06-21 01:09:05 +0200

    add chapters services and routing

--- Faithflow/src/App.jsx
+++ Faithflow/src/App.jsx
@@ -1,51 +1,15 @@
-import { useState, useEffect } from "react";
-import { keywordService } from "./service/keyword.js";
+import { Route, Routes } from "react-router-dom";
+import Home from "./pages/Home.jsx";
+import SearchPage from "./pages/SearchPage.jsx";
 
 function App() {
-  const [search, setSearch] = useState("");
-  const [result, setResult] = useState("");
-
-  const handleSubmit = async (e) => {
-    e.preventDefault();
-    const data = await keywordService(search);
-    setResult(data);
-    console.log(data);
-  };
-
-  const handleChange = (e) => {
-    e.preventDefault();
-    setSearch(e.target.value);
-  };
   return (
-    <>
-      <h1 className="text-3xl font-bold underline">Hello world!</h1>
-      <form
-        className="flex justify-center items-center"
-        onSubmit={handleSubmit}
-      >
-        <label className="font-normal mr-2" htmlFor="search">
-          Insert Keyword
-        </label>
-        <input
-          type="text"
-          id="search"
-          name="keyword"
-          placeholder="Jesus"
-          onChange={handleChange}
-        />
-        <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold rounded focus:outline-none focus:shadow-outline w-24 m-2">
-          Search
-        </button>
-      </form>
-      <div className="flex flex-col justify-center items-center gap-2">
-        {result &&
-          result.map((item) => (
-            <p className="w-96 " key={item.id}>
-              {item.text}
-            </p>
-          ))}
-      </div>
-    </>
+    <div className="bg-[url('/background.jpg')] bg-cover bg-center bg-no-repeat min-h-screen overflow-hidden">
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/passage" element={<SearchPage />} />
+      </Routes>
+    </div>
   );
 }
 
