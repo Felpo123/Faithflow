8ce654f015d2fda3d144ecc26d2e187d1e5fd265
Tomás Sarmiento <t.sarmiento01@ufromail.cl>
2023-06-23 14:05:34 -0400

    UI enhancement on search page

--- Faithflow/src/pages/SearchPage.jsx
+++ Faithflow/src/pages/SearchPage.jsx
@@ -25,50 +25,66 @@ function SearchPage() {
   return (
     
     <div className="max-w-4xl my-0 mx-auto">
-      <NavBar></NavBar>
+      <NavBar/>
       <form
-        className="flex justify-center items-center"
+        className="flex flex-col justify-center items-center"
         onSubmit={handleSubmit}
       >
-        <label className="font-normal mr-2 text-white" htmlFor="search">
-          Insert Keyword
-        </label>
-        <input
-          type="text"
-          id="search"
-          name="keyword"
-          placeholder="chapter"
-          onChange={handleChange}
-        />
-        <input
-          type="text"
-          id="book"
-          name="book"
-          placeholder="book"
-          onChange={(e) => {
-            e.preventDefault();
-            setBook(e.target.value);
-          }}
-        />
-        <input
-          type="text"
-          id="version"
-          name="version"
-          placeholder="version"
-          onChange={(e) => {
-            e.preventDefault();
-            setVersion(e.target.value);
-          }}
-        />
-
-        <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold rounded focus:outline-none focus:shadow-outline w-24 m-2">
+        <div className="flex justify-center items-center">
+          <div className='flex flex-col justify-center items-center'>
+            <label className="font-normal max-w-1 mr-2 text-white" htmlFor="search">
+              Chapter
+            </label>
+            <input
+              type="text"
+              id="search"
+              name="keyword"
+              placeholder="01"
+              onChange={handleChange}
+              className="rounded-[3px] w-20 m-2 px-2"
+              />
+          </div>
+          <div className='flex flex-col justify-center items-center'>
+          <label className="font-normal max-w-1 mr-2 text-white" htmlFor="search">
+              Book
+            </label>
+            <input
+              type="text"
+              id="book"
+              name="book"
+              placeholder="01"
+              onChange={(e) => {
+                e.preventDefault();
+                setBook(e.target.value);
+              }}
+              className="rounded-[3px] w-20 m-2 px-2"
+              />
+          </div>
+          <div className='flex flex-col justify-center items-center'>
+            <label className="font-normal max-w-1 mr-2 text-white" htmlFor="search">
+              Chapter
+            </label>
+            <input
+              type="text"
+              id="version"
+              name="version"
+              placeholder="kjv"
+              onChange={(e) => {
+                e.preventDefault();
+                setVersion(e.target.value);
+              }}
+              className="rounded-[3px] w-20 m-2 px-2"
+              />
+          </div>
+        </div>
+        <button className="transition-all bg-blue-500 hover:bg-blue-700 text-white font-bold rounded focus:outline-none focus:shadow-outline w-24 h-full m-2">
           Search
         </button>
       </form>
-      <div className="flex flex-col justify-center items-center gap-2">
+      <div className="flex flex-col justify-center items-center gap-2 p-5">
         {result &&
           result.map((item) => (
-            <p className="w-96 text-white" key={item.id}>
+            <p className="w-[80%] text-white" key={item.id}>
               <b>{item.verse}</b> {item.text}
             </p>
           ))}
