b7f4116f90c170233728939ad83907332aa6e5b0
Felipe Castro <77942887+Felpo123@users.noreply.github.com>
2023-06-27 02:48:12 +0200

    add keyword form on navbar

--- Faithflow/src/components/NavBar.jsx
+++ Faithflow/src/components/NavBar.jsx
@@ -1,13 +1,35 @@
+import Form from "./Form";
 
-export default function NavBar(){
-    return(
-      <header className="flex items-center justify-start gap-4">
-        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" 
-           fill="white"className="w-12 h-12 pt-6"><path fillRule="evenodd" 
-           d="M3 5.25a.75.75 0 01.75-.75h16.5a.75.75 0 010 1.5H3.75A.75.75 0 013 5.25zm0 4.5A.75.75 0 013.75 
+export default function NavBar({ setShowKeyword }) {
+  return (
+    <div className="flex items-center justify-center">
+      <header className="flex items-center justify-around w-3/4 gap-96">
+        <div className="flex item-center justify-center gap-4">
+          <div className="w-6 h-6 pt-8">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 24 24"
+              fill="white"
+            >
+              <path
+                fillRule="evenodd"
+                d="M3 5.25a.75.75 0 01.75-.75h16.5a.75.75 0 010 1.5H3.75A.75.75 0 013 5.25zm0 4.5A.75.75 0 013.75 
            9h16.5a.75.75 0 010 1.5H3.75A.75.75 0 013 9.75zm0 4.5a.75.75 0 01.75-.75h16.5a.75.75 0 010 1.5H3.75a.75.75 
-           0 01-.75-.75zm0 4.5a.75.75 0 01.75-.75h16.5a.75.75 0 010 1.5H3.75a.75.75 0 01-.75-.75z" clipRule="evenodd"/></svg>
-        <img src="public\faithflow.png" alt="Faithflow Logo" className="w-28 h-16 aspect-auto pt-2"/>
-        </header>
-    )
-}       
+           0 01-.75-.75zm0 4.5a.75.75 0 01.75-.75h16.5a.75.75 0 010 1.5H3.75a.75.75 0 01-.75-.75z"
+                clipRule="evenodd"
+              />
+            </svg>
+          </div>
+          <img
+            src="public\faithflow.png"
+            alt="Faithflow Logo"
+            className="w-28 h-16 aspect-auto pt-2"
+          />
+        </div>
+        <div>
+          <Form setShowKeyword={setShowKeyword} />
+        </div>
+      </header>
+    </div>
+  );
+}
